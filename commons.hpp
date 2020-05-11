2caa7f8339d4ea7ec8bafd5142f678d682e22562
육심현 <simoncreek@tutanota.com>
2020-05-11 22:25:48 +0900

    Included miscellaneous classes into common header

--- commons.hpp
+++ commons.hpp
@@ -3,6 +3,22 @@
 
 #include <utility>
 
+enum class DIRECTION{
+	UP = 2,
+	RIGHT = 1,
+	DOWN = -2,
+	LEFT = -1
+};
+
+// 2 -> 0, 1 : value/2(0) -> y, value %2(1) -> x; -=value/2 , -=value%2 * 3 
+// 1 -> 1, 0 value/2 -> y, value %2 -> x; -=value/2 , -=value%2 * 3
+// -2 -> 0 -1
+// -1 -> -1 0
+enum class KEYINPUT{
+	RIGHT = -1,
+	LEFT = 1,
+};
+
 class Position{
 	private:
 		int x_pos;
