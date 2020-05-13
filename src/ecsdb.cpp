297cce2dd8f8c499e16af24070ce803db95d2392
육심현 <simoncreek@tutanota.com>
2020-05-14 01:33:26 +0900

    주석 추가

--- src/ecsdb.cpp
+++ src/ecsdb.cpp
@@ -8,18 +8,19 @@
 
 // Functions	
 // Getter
-const Time& ECSDB::get_time(){ return runtime; }
+const long ECSDB::get_time() const { return start_time; }
+const std::pair<int, int> ECSDB::get_measure() const { return std::pair<int, int>(width, height); }
 std::vector<PlayerBody>& ECSDB::get_snake(){ return snake; }
-const bool ECSDB::get_death(){return player_death;}
-const PosVc& ECSDB::get_walls(){return walls;}
-const Portal& ECSDB::get_portal(){return portal;}
-const ItmVc& ECSDB::get_growth(){return growth;}
-const ItmVc& ECSDB::get_posion(){return poison;}
+PlayerBody& ECSDB::get_head(){return snake[0];}
+const bool ECSDB::get_death() const{return player_death;}
+const PosVc& ECSDB::get_walls() const{return walls;}
+const Portal& ECSDB::get_portal() const{return portal;}
+ItmVc& ECSDB::get_growth(){return growth;}
+ItmVc& ECSDB::get_posion(){return poison;}
 const std::queue<char> ECSDB::get_inputs(){return console_inputs;}
 // Setter
-void ECSDB::set_time(Time time){ runtime = time;	} 
 void ECSDB::set_death(bool value){ player_death = value; }
 void ECSDB::set_snake(bool increment){ }
 void ECSDB::set_portal(Position first, Position second){}
-void ECSDB::set_growth(bool increment, int entity_index){}
-void ECSDB::set_poison(bool increment, int entity_index){}
+void ECSDB::set_growth(Position){}
+void ECSDB::set_poison(Position){}
