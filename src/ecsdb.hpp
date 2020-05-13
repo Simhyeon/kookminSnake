d3ac0321e47f931a2199769ee5a777043649719d
simon <aracus755@gmail.com>
2020-05-13 16:53:41 +0900

    DB에 타임스탬프 플레이스홀더 추가 및 아이템 컴포넌트 변경에 따른 변수 타입 변경

--- src/ecsdb.hpp
+++ src/ecsdb.hpp
@@ -2,38 +2,44 @@
 #define ECSDB_H
 
 #include "commons.hpp"
+#include "item.hpp"
+#include "portal.hpp"
 #include "playerbody.hpp"
 #include <vector>
 #include <queue>
 
 class ECSDB{
 private:
+	Time runtime;
+	const int item_size = 3;
 	const int width, height;
 	bool player_death;
 	std::vector<PlayerBody> snake;
 	PosVc walls;
 	PosVc empty;
-	PosVc portals;
-	PosVc goodies;
-	PosVc poison;
+	Portal portal;
+	ItmVc growth;
+	ItmVc poison;
 
 	std::queue<char> console_inputs; // Placeholder
 public:
 	ECSDB();
 
+	const Time& get_time();
 	std::vector<PlayerBody>& get_snake();
 	const bool get_death();
 	const PosVc& get_walls();
 	const PosVc& get_portals();
-	const PosVc& get_goodies();
-	const PosVc& get_posions();
+	const ItmVc& get_growth();
+	const ItmVc& get_posion();
 
 	const std::queue<char> get_inputs(); // Placeholder
 
+	void set_time(Time time);
 	void set_death(bool value);
 	void set_snake(bool increment);
-	void set_portal(bool increment, int entity_index);
-	void set_goodies(bool increment, int entity_index);
+	void set_portal(Position first, Position second);
+	void set_growth(bool increment, int entity_index);
 	void set_poison(bool increment, int entity_index);
 
 	void set_input(char input); // Placeholder
