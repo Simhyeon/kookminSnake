cc466ca1d113c9c9e8f30c754335b13dfd3e0927
육심현 <simoncreek@tutanota.com>
2020-05-27 20:23:07 +0900

    2차원 캐릭터 배열로 만든 뱀 맵 추가, empty 벡터와 snake_map 과 관련된 함수들 추가, empty 벡터를 확정하는 update 함수 추가(임시)

--- src/ecsdb.cpp
+++ src/ecsdb.cpp
@@ -3,24 +3,151 @@
 #include "commons.hpp"
 #include "item.hpp"
 #include "playerbody.hpp"
+#include <cstdio>
 #include <vector>
 #include <queue>
+#include <iostream>
+#include <algorithm>
+
+constexpr int ECSDB::width;
+constexpr int ECSDB::height;
 
 // Functions	
+// Constructor
+// Currently for Debuggin
+ECSDB::ECSDB() : start_time(Util::get_time()), portal(Position(0, 1), Position(1,1)){
+	// Make this as function for fuck's sake
+	for (int i =0; i < width; i++){
+		for (int j =0; j < height; j++){
+			snake_map[i][j] = '`';
+		}
+	}
+	snake = std::vector<PlayerBody>();
+
+	for (int j =0; j < height; j++){
+		for (int i =0; i < width; i++){
+			if (j == 0 || j == height-1)	{
+				snake_map[i][j] = 'w';
+				walls.push_back(Position(i,j));
+			} else {
+				snake_map[0][j] = 'w';
+				snake_map[width-1][j] = 'w';
+				walls.push_back(Position(0,j));
+				walls.push_back(Position(width-1,j));
+				break;
+			}
+		}
+	}
+
+	for(int i =0; i < width; i++){
+		for(int j =0; j < height; j++){
+			switch (snake_map[i][j]){
+				case '`':
+					empty.push_back(Position(i,j));
+					break;
+				case 'w':
+					break;
+				case '#':
+					break;
+				case '@':
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
 // Getter
 const long ECSDB::get_time() const { return start_time; }
-const std::pair<int, int> ECSDB::get_measure() const { return std::pair<int, int>(width, height); }
+const std::pair<int, int> ECSDB::get_measure() { return std::pair<int, int>(ECSDB::width, ECSDB::height); }
 std::vector<PlayerBody>& ECSDB::get_snake(){ return snake; }
+void ECSDB::update_snake_map(){
+	// Refresh window
+	for (int i =0; i < width; i++){
+		for (int j =0; j < height; j++){
+			snake_map[i][j] = '`';
+		}
+	}
+	
+	// Set all items
+	for (auto it = growth.begin(); it != growth.end(); it++){
+		snake_map[(*it).get_pos().get_x()][(*it).get_pos().get_y()] = '+';
+	}
+
+	for (auto it = poison.begin(); it != poison.end(); it++){
+		snake_map[(*it).get_pos().get_x()][(*it).get_pos().get_y()] = '-';
+	}
+
+	// Set all walls
+	for (int j =0; j < height; j++){
+		for (int i =0; i < width; i++){
+			if (j == 0 || j == height-1)	{
+				snake_map[i][j] = 'w';
+			} else {
+				snake_map[0][j] = 'w';
+				snake_map[width-1][j] = 'w';
+				break;
+			}
+		}
+	}
+
+	// Set all player characters
+	snake_map[get_head().get_pos().get_x()][get_head().get_pos().get_y()] = '@';
+	for (auto it = snake.begin() + 1; it != snake.end(); it++){
+		snake_map[(*it).get_pos().get_x()][(*it).get_pos().get_y()] = '#';
+	}
+
+	// Update for empty class
+	empty = PosVc{};
+	for(int i =0; i < width; i++){
+		for(int j =0; j < height; j++){
+			switch (snake_map[i][j]){
+				case '`':
+					empty.push_back(Position(i,j));
+					break;
+				case 'w':
+					break;
+				case '#':
+					break;
+				case '@':
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
 PlayerBody& ECSDB::get_head(){return snake[0];}
 const bool ECSDB::get_death() const{return player_death;}
 const PosVc& ECSDB::get_walls() const{return walls;}
 const Portal& ECSDB::get_portal() const{return portal;}
 ItmVc& ECSDB::get_growth(){return growth;}
-ItmVc& ECSDB::get_posion(){return poison;}
+ItmVc& ECSDB::get_poison(){return poison;}
+PosVc& ECSDB::get_empty(){
+	return empty;
+}
+
 const std::queue<char> ECSDB::get_inputs(){return console_inputs;}
 // Setter
 void ECSDB::set_death(bool value){ player_death = value; }
 void ECSDB::set_snake(bool increment){ }
-void ECSDB::set_portal(Position first, Position second){}
-void ECSDB::set_growth(Position){}
-void ECSDB::set_poison(Position){}
+void ECSDB::set_portal(Portal portal){
+	this->portal = portal;
+}
+
+void ECSDB::set_growth(Position position){
+	growth.push_back(Item(position));
+	empty.erase(std::remove(empty.begin(), empty.end(), position), empty.end());
+}
+
+void ECSDB::set_poison(Position position){
+	poison.push_back(Item(position));
+	empty.erase(std::remove(empty.begin(), empty.end(), position), empty.end());
+}
+
+void ECSDB::set_snake_map(Position pos, char ch){
+	snake_map[pos.get_x()-1][pos.get_y()-1] = ch;
+}
+void ECSDB::remove_empty(Position pos){
+	empty.erase(std::remove(empty.begin(), empty.end(), pos), empty.end());
+}
--- src/ecsdb.hpp
+++ src/ecsdb.hpp
@@ -18,17 +18,17 @@ private:
 	/** \brief 게임 화면의 너비
 	 *	점수판의 크기와 너비와 동일하다.
 	 */
-	const int width;
+	static constexpr int width = 10;
 
 	/** \brief 게임 화면의 크기
 	 *	점수판의 크기와 너비와 동일하다.
 	 */
-	const int height;
+	static constexpr int height = 10;
 
 	/** \brief 플레이어의 죽음 여부를 저장하는 변수
 	 *	
 	 */
-	bool player_death;
+	bool player_death= false;
 
 	/** \brief 플레이어 몸체 컴포넌트
 	 *	
@@ -38,12 +38,12 @@ private:
 	/** \brief 벽 컴포넌트
 	 *	
 	 */
-	PosVc walls;
+	PosVc walls = PosVc{};
 
 	/** \brief 빈 공간 컴포넌트
 	 *	
 	 */
-	PosVc empty;
+	PosVc empty = PosVc{};
 
 	/** \brief 포탈 컴포넌트
 	 *	
@@ -53,34 +53,45 @@ private:
 	/** \brief 크기 증가 아이템 컴포넌트
 	 *	
 	 */
-	ItmVc growth;
+	ItmVc growth = ItmVc{};
 
 	/** \brief 크기 감소 아이템 컴포넌트
 	 *	
 	 */
-	ItmVc poison;
+	ItmVc poison = ItmVc{};
 
 	std::queue<char> console_inputs; // Placeholder
 public:
+
+	/** \brief 렌더 정보를 담는 char 2차 배열
+	 *	2차원 배열이 아닌 다른 걸로 구현하는 것이 더 편하고 빠르다.
+	 */
+	char snake_map[width][height];
+
 	ECSDB();
 
 	const long get_time() const;
-	const std::pair<int, int> get_measure() const;
+	const std::pair<int, int> get_measure();
+	//const char** get_snakemap() const;
+	void update_snake_map();
 	std::vector<PlayerBody>& get_snake();
 	PlayerBody& get_head();
 	const bool get_death() const;
 	const PosVc& get_walls() const;
 	const Portal& get_portal() const;
 	ItmVc& get_growth();
-	ItmVc& get_posion();
+	ItmVc& get_poison();
+	PosVc& get_empty();
 
 	const std::queue<char> get_inputs(); // Placeholder
 
 	void set_death(bool value);
 	void set_snake(bool increment);
-	void set_portal(Position first, Position second);
+	void set_portal(Portal);
 	void set_growth(Position);
 	void set_poison(Position);
+	void set_snake_map(Position, char);
+	void remove_empty(Position);
 
 	void set_input(char input); // Placeholder
 };
