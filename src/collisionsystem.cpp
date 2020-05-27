60ad843b70dedd3673bd0544499a8c92d9364ce9
육심현 <simoncreek@tutanota.com>
2020-05-27 20:12:55 +0900

    주석 추가, 사실 의미는 불명이다.

--- src/collisionsystem.cpp
+++ src/collisionsystem.cpp
@@ -7,6 +7,7 @@ void CollisionSystem::Process(ECSDB & db){
 	}
 }
 
+// 현재 Wall의 Position에 저장된 정보가 0 인덱스부터 시작한다. 이 점 유의하자. 
 bool CollisionSystem::DetectCollision(const PosVc& walls,const std::vector<PlayerBody>& bodies){
 	const PlayerBody& head = bodies[0];
 	for (const Position& wall: walls){
