3b5d057c01b55206bb6933a63822844b4b7ed5df
육심현 <simoncreek@tutanota.com>
2020-05-27 20:09:37 +0900

    Playerbody 클래스 내용 구현

--- src/playerbody.cpp
+++ src/playerbody.cpp
@@ -1,10 +1,26 @@
 #include "playerbody.hpp"
+#include "commons.hpp"
 
 PlayerBody::PlayerBody(){}
-PlayerBody::PlayerBody(DIRECTION, Position){}
-const DIRECTION PlayerBody::get_direction(){}
-void PlayerBody::set_direction(DIRECTION){}
-const Position PlayerBody::get_pos() const{}
-void PlayerBody::increment_pos(int x, int y){}
-void PlayerBody::set_pos(const Position){}
-void PlayerBody::set_pos(int x, int y){}
+PlayerBody::PlayerBody(DIRECTION dir, Position position){
+	direction = dir;
+	pos = position;
+}
+const DIRECTION PlayerBody::get_direction(){
+	return direction;
+}
+void PlayerBody::set_direction(DIRECTION dir){
+	direction = dir;
+}
+const Position PlayerBody::get_pos() const{
+	return pos;
+}
+void PlayerBody::increment_pos(int x, int y){
+	pos.increment(x, y);
+}
+void PlayerBody::set_pos(const Position position){
+	pos = position;
+}
+void PlayerBody::set_pos(int x, int y){
+	pos = Position(x,y);
+}
