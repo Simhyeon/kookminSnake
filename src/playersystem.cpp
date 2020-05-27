2cf77267a493362254cfae5389235b104f770915
육심현 <simoncreek@tutanota.com>
2020-05-27 20:11:38 +0900

    process 함수에 db 업데이트 로직 추가, move 함수에서 cache가 제대로 값을 저장하지 않는 내용 수정

--- src/playersystem.cpp
+++ src/playersystem.cpp
@@ -32,17 +32,20 @@ void PlayerBodySystem::process(ECSDB& db) {
 		}
 	}
 	move(db.get_snake());
+	db.update_snake_map();
 }
 
+// Undefined
 std::pair<bool, DIRECTION> PlayerBodySystem::get_console_input(char input) {
 	// Check if input is valid
 }
 
 void PlayerBodySystem::move(std::vector<PlayerBody>& bodies){
-	int dirct = static_cast<int>(bodies[0].get_direction());
-	bodies[0].increment_pos(dirct%2, -dirct/2);
 	PlayerBody parent = bodies[0];
 	PlayerBody cache;
+
+	int dirct = static_cast<int>(bodies[0].get_direction());
+	bodies[0].increment_pos(dirct%2, -dirct/2);
 
 	// Should check if operation is optimal and change Position class implementation accordingly.
 	for(auto it = bodies.begin() + 1; it != bodies.end(); it++) {
