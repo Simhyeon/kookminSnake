445bbfbd4fd0cd54e66ce82c6b99651d3cdeaa57
육심현 <simoncreek@tutanota.com>
2020-05-29 00:48:29 +0900

    PlayerBody의 클래스의 방향을 단이 변수가 아닌 큐로 변경함. 이를 통해서 플레이어 시스템이 컴포넌트를 독립적으로 순환하면서 연산할 수 있게 됨. 기존의 방법은 컴포넌트간 비교 연산이 필요했음. 한편 큐로 변경하면서 여러 접근자를 추가함

--- src/playerbody.cpp
+++ src/playerbody.cpp
@@ -1,16 +1,33 @@
 #include "playerbody.hpp"
 #include "commons.hpp"
+#include <iostream>
 
 PlayerBody::PlayerBody(){}
-PlayerBody::PlayerBody(DIRECTION dir, Position position){
-	direction = dir;
+PlayerBody::PlayerBody(Position position, DIRECTION direction, unsigned int body_size){
 	pos = position;
+	dir_queue = std::deque<DIRECTION>(std::queue<DIRECTION>::container_type(body_size, direction));
 }
+PlayerBody::PlayerBody(Position position, std::deque<DIRECTION> copy){
+	pos = position;
+	dir_queue = copy;
+}
+// const 왜 넣은 거지
 const DIRECTION PlayerBody::get_direction(){
-	return direction;
+	return dir_queue.front();
 }
-void PlayerBody::set_direction(DIRECTION dir){
-	direction = dir;
+
+DIRECTION PlayerBody::pop_direction(){
+	DIRECTION dir = dir_queue.front();
+	dir_queue.pop_front();
+	return dir;
+}
+
+void PlayerBody::push_direction(DIRECTION dir){
+	dir_queue.push_back(dir);
+}
+
+void PlayerBody::push_direction_front(DIRECTION dir){
+	dir_queue.push_front(dir);
 }
 const Position PlayerBody::get_pos() const{
 	return pos;
--- src/playerbody.hpp
+++ src/playerbody.hpp
@@ -2,21 +2,26 @@
 #define PLAYERBODY_H
 
 #include "commons.hpp"
-
+#include <queue>
 
 class PlayerBody{
 private:
-	DIRECTION direction;
+	std::deque<DIRECTION> dir_queue;
 	Position pos;
 public:
 	PlayerBody();
-	PlayerBody(DIRECTION, Position);
+	PlayerBody(Position, DIRECTION direction, unsigned int body_size);
+	PlayerBody(Position, std::deque<DIRECTION> copy);
 	const DIRECTION get_direction();
-	void set_direction(DIRECTION);
+	DIRECTION pop_direction();
+	void push_direction(DIRECTION);
+	void push_direction_front(DIRECTION dir);
 	const Position get_pos() const;
 	void increment_pos(int x, int y);
 	void set_pos(const Position);
 	void set_pos(int x, int y);
+
+	std::deque<DIRECTION> get_queue() {return dir_queue;}
 };
 
 #endif
