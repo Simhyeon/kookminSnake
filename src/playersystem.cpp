afed5c4488bba36bffe9c0c09ca57b3f04862243
육심현 <simoncreek@tutanota.com>
2020-06-01 05:26:27 +0900

    empty구조 변화로 시스템 수정

--- src/playersystem.cpp
+++ src/playersystem.cpp
@@ -23,19 +23,21 @@ void PlayerBodySystem::process(ECSDB& db) {
 		return;
 	}
 
-	DIRECTION next = db.last_direction;
+	DIRECTION next = db.get_last_direction();
 
-	if (static_cast<int>(db.last_direction) != -static_cast<int>(next)){
-		change_direction(next, db.get_snake());
+	if (Util::get_dir_int(db.get_head().get_direction()) != -Util::get_dir_int(next)){
+		change_direction(next, db.get_mut_snake());
 	} else {
 		db.set_death(true);
 	}
 
-	move(db.get_snake());
-	// Update method should be changed according to new data structure
-	db.update_snake_map();
+	Position last_pos = db.get_snake()[db.get_snake().size() -1].get_pos(); // 이걸 활용할 예정이었는데 어떻게 할까... 흐음... 
+	db.set_empty(last_pos.get_x(), last_pos.get_y(), FILL::EMPTY);
+	move(db.get_mut_snake());
+	db.set_empty(db.get_snake()[0].get_pos().get_x(), db.get_snake()[0].get_pos().get_y(), FILL::FILL);
 }
 
+// TODO
 // Undefined
 std::pair<bool, DIRECTION> PlayerBodySystem::get_console_input(char input) {
 	// Check if input is valid
