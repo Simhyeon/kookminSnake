0d89ff65cf0ef15d778abd03c5df5c6e1904e872
육심현 <simoncreek@tutanota.com>
2020-06-02 00:50:35 +0900

    포탈 로직을 프로젝트 조건에 맞게 다시 수정, 게이트 이동시에 방향이 정해지도록 수정

--- src/code/portal.cpp
+++ src/code/portal.cpp
@@ -2,26 +2,8 @@
 #include "commons.hpp"
 #include <chrono>
 
-GateEntry::GateEntry(){ }
-
-GateEntry::GateEntry(const Position& pos, DIRECTION dir){
-	position= pos; 
-	direction = dir;
-}
-void GateEntry::set_direction(DIRECTION dir){
-	direction= dir;
-}
-
-Position GateEntry::get_position(){
-	return position;
-}
-
-DIRECTION GateEntry::get_direction(){
-	return direction;
-}
-
-Portal::Portal(){ }
-Portal::Portal(const Position& first,const Position& second, const DIRECTION first_dir, DIRECTION second_dir): timestamp(Util::get_time()), first_entry(first, first_dir), second_entry(second, second_dir) { }
+Portal::Portal() : timestamp(Util::get_time()){ }
+Portal::Portal(const Position& first,const Position& second, const DIRECTION first_dir, DIRECTION second_dir): timestamp(Util::get_time()), first_entry(first), second_entry(second) { }
 
 Portal::Portal(const Portal& portal): timestamp(portal.timestamp), first_entry(portal.first_entry), second_entry(portal.second_entry){ }
 
@@ -32,17 +14,9 @@ Portal& Portal::operator=(Portal& port){
 	return *this;
 }
 Position Portal::get_first_pos() const{
-	return first_entry.position;
+	return first_entry;
 }
 
 Position Portal::get_second_pos() const{
-	return second_entry.position;
-}
-
-DIRECTION Portal::get_first_dir() const{
-	return first_entry.direction;
-}
-
-DIRECTION Portal::get_second_dir() const{
-	return second_entry.direction;
+	return second_entry;
 }
--- src/code/portal.hpp
+++ src/code/portal.hpp
@@ -3,25 +3,11 @@
 
 #include "commons.hpp"
 
-class GateEntry{
-private:
-	Position position;
-	DIRECTION direction;
-public:
-	GateEntry();
-	GateEntry(const Position& pos, DIRECTION dir);
-	void set_direction(DIRECTION dir); 
-	Position get_position();
-	DIRECTION get_direction();
-
-friend class Portal;
-};
-
 class Portal{
 private:
 	long timestamp;
-	GateEntry first_entry;
-	GateEntry second_entry;
+	Position first_entry;
+	Position second_entry;
 public:
 	Portal();
 	Portal(const Position& first,const Position& second, const DIRECTION first_dir, DIRECTION second_dir);
@@ -30,8 +16,6 @@ public:
 	Portal& operator=(Portal&);
 	Position get_first_pos() const;
 	Position get_second_pos() const;
-	DIRECTION get_first_dir() const;
-	DIRECTION get_second_dir() const;
 
 friend class PortalSystem;
 };
