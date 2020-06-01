f56caf38c205fef29df40a13f074f03669f804ba
simon <aracus755@gmail.com>
2020-06-01 15:47:19 +0900

    Portal에 GateEntry를 추가, 나중에 Portal도 Gate로 바꿀 예정

--- src/portal.cpp
+++ src/portal.cpp
@@ -1,21 +1,48 @@
 #include "portal.hpp"
+#include "commons.hpp"
 #include <chrono>
 
-Portal::Portal(const Position& first,const Position& second): timestamp(Util::get_time()), first(first), second(second) { }
-//Portal::Portal(const Position& first, const Position& second): timestamp(Util::get_time()), first(first), second(second) { }
+GateEntry::GateEntry(){ }
 
-Portal::Portal(const Portal& portal): timestamp(portal.timestamp), first(portal.first), second(portal.second){ }
+GateEntry::GateEntry(const Position& pos, DIRECTION dir){
+	position= pos; 
+	direction = dir;
+}
+void GateEntry::set_direction(DIRECTION dir){
+	direction= dir;
+}
+
+Position GateEntry::get_position(){
+	return position;
+}
+
+DIRECTION GateEntry::get_direction(){
+	return direction;
+}
+
+Portal::Portal(){ }
+Portal::Portal(const Position& first,const Position& second, const DIRECTION first_dir, DIRECTION second_dir): timestamp(Util::get_time()), first_entry(first, first_dir), second_entry(second, second_dir) { }
+
+Portal::Portal(const Portal& portal): timestamp(portal.timestamp), first_entry(portal.first_entry), second_entry(portal.second_entry){ }
 
 Portal& Portal::operator=(Portal& port){
 	timestamp = port.timestamp; 
-	first = port.first;
-	second = port.second;
+	first_entry = port.first_entry;
+	second_entry = port.second_entry;
 	return *this;
 }
-Position Portal::get_first_pos(){
-	return first;
+Position Portal::get_first_pos() const{
+	return first_entry.position;
 }
 
-Position Portal::get_second_pos(){
-	return second;
+Position Portal::get_second_pos() const{
+	return second_entry.position;
+}
+
+DIRECTION Portal::get_first_dir() const{
+	return first_entry.direction;
+}
+
+DIRECTION Portal::get_second_dir() const{
+	return second_entry.direction;
 }
--- src/portal.hpp
+++ src/portal.hpp
@@ -3,18 +3,35 @@
 
 #include "commons.hpp"
 
+class GateEntry{
+private:
+	Position position;
+	DIRECTION direction;
+public:
+	GateEntry();
+	GateEntry(const Position& pos, DIRECTION dir);
+	void set_direction(DIRECTION dir); 
+	Position get_position();
+	DIRECTION get_direction();
+
+friend class Portal;
+};
+
 class Portal{
 private:
 	long timestamp;
-	Position first;
-	Position second;
+	GateEntry first_entry;
+	GateEntry second_entry;
 public:
-	Portal(const Position&, const Position&);
+	Portal();
+	Portal(const Position& first,const Position& second, const DIRECTION first_dir, DIRECTION second_dir);
 	//Portal(const Position&, const Position&);
 	Portal(const Portal&);
 	Portal& operator=(Portal&);
-	Position get_first_pos();
-	Position get_second_pos();
+	Position get_first_pos() const;
+	Position get_second_pos() const;
+	DIRECTION get_first_dir() const;
+	DIRECTION get_second_dir() const;
 
 friend class PortalSystem;
 };
