5da91d201eab907a741f1812e214d4e89161b2eb
육심현 <simoncreek@tutanota.com>
2020-06-06 21:42:50 +0900

    여러 조건을 확인할 수 있도록 코드를 수정

--- src/code/scoreboard.cpp
+++ src/code/scoreboard.cpp
@@ -1,9 +1,26 @@
 #include "scoreboard.hpp"
 
 void ScoreSystem::process(ECSDB &db){
-	if (db.get_growth_qual() <= db.get_growth_counter() &&
-			db.get_poison_qual() <= db.get_poison_counter())
-			/*&& db.get_gate_qual() <= db.get_gate_counter())*/{
+
+	if (db.get_growth_qual() <= db.get_growth_counter()){
+		db.set_growth_ok(true);
+	}
+
+	if (db.get_poison_qual() <= db.get_poison_counter()){
+		db.set_poison_ok(true);
+	}
+
+	if (db.get_length_qual() <= db.get_snake().size()){
+		db.set_length_ok(true);
+	} else {
+		db.set_length_ok(false);
+	}
+
+	if (db.get_gate_qual() <= db.get_gate_counter()){
+		db.set_gate_ok(true);
+	}
+
+	if (db.get_length_ok() && db.get_growth_ok() && db.get_poison_ok() && db.get_gate_ok()){
 		db.set_sucess(true);
 	}
 }
