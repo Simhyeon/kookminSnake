146284b6408275db27c483d8ee8e39bdfcd6ca80
HELLOHIDI <fbgmlwo123@naver.com>
2020-06-08 16:40:47 +0900

    pause

--- src/code/inputmanager.cpp
+++ src/code/inputmanager.cpp
@@ -21,6 +21,19 @@ void InputManager::process(ECSDB& db){
 			case 'd' : 
 	            db.set_last_direction(DIRECTION::RIGHT);
 			break;
+
+			case 'P':
+			case 'p': 
+			char c;
+			wattron(db.get_playboard(), A_BOLD);
+			mvwprintw(db.get_playboard(), db.get_height()/2, db.get_width()/2, "PAUSE");
+			wattroff(db.get_playboard(), A_BOLD);
+			nodelay(db.get_playboard(), FALSE);
+			do{
+				c = wgetch(db.get_playboard());
+			}while(c!='p' && c!='P');
+			nodelay(db.get_playboard(), TRUE);
+			break;
 			
 			default:
 			   break;
