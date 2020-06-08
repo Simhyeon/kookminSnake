3c1b86b7231869b317a7fb70ad3e9bac14c02c83
HELLOHIDI <fbgmlwo123@naver.com>
2020-06-08 16:35:36 +0900

    color

--- src/code/renderer.cpp
+++ src/code/renderer.cpp
@@ -9,6 +9,13 @@ Renderer::Renderer(){}
 void Renderer::init(ECSDB& ecsdb){
 
 	initscr();
+	start_color();
+	init_pair(1, COLOR_BLUE, COLOR_BLACK); //map_color
+	init_pair(2, COLOR_GREEN,COLOR_BLACK); //boundary_color
+	init_pair(3, COLOR_YELLOW,COLOR_BLACK); //growth_Color
+	init_pair(4, COLOR_RED, COLOR_BLACK);  //poison_color
+	init_pair(5, COLOR_WHITE , COLOR_BLACK); //portal_Color
+
 	noecho(); //no echo for the key pressed
 	cbreak();
 	curs_set(1);  //no cursor show
@@ -32,6 +39,7 @@ void Renderer::process(ECSDB & ecsdb){ //STARTGAME
 }
 
 void Renderer::draw(ECSDB& db){
+	
 	WINDOW* win = db.get_playboard();
 	werase(win);
 	//box(win,0,0);
@@ -40,7 +48,29 @@ void Renderer::draw(ECSDB& db){
 	for (int i =0; i< db.get_width(); i++){
 		for (int j =0; j< db.get_height(); j++){
 			ch = snake_map[j][i];
-			mvwaddch(win, i*2, j*2, ch);
+			switch(ch){
+				case '*':
+				mvwaddch(win, i*2, j*2, ch | COLOR_PAIR(2));
+				break;
+
+				case 'P':
+				mvwaddch(win, i*2, j*2, ch | COLOR_PAIR(3));
+				break;
+
+				case 'X':
+				mvwaddch(win, i*2, j*2, ch | COLOR_PAIR(4));
+				break;
+
+				case 'O':
+				mvwaddch(win, i*2, j*2, ch | COLOR_PAIR(5));
+				break;
+				
+
+				default:
+				mvwaddch(win, i*2, j*2, ch | COLOR_PAIR(1));
+				break;
+			}
+			
 		}
 	}
 	wrefresh(win);
@@ -49,30 +79,32 @@ void Renderer::draw(ECSDB& db){
 
 void Renderer::printScore(ECSDB& db){
 	WINDOW* score = db.get_scoreboard();
+	
 	werase(score);
 	//mvwprintw(score, 0, 0,  "Time: %ld", Util::get_time());
-	mvwprintw(score, 0, 0,  "length: %d/%d (%c)", 
+	mvwprintw(score, 1, 1,  "length: %d/%d (%c)", 
 			db.get_snake().size(), 
 			db.get_length_qual(),
 			db.get_length_ok() ? 'O' : 'X'
 			);
-	mvwprintw(score, 1, 0,  "growth: %d/%d (%c)", 
+	mvwprintw(score, 2, 1,  "growth: %d/%d (%c)", 
 			db.get_growth_counter(),
 			db.get_growth_qual(),
 			db.get_growth_ok() ? 'O' : 'X'
 			);
-	mvwprintw(score, 2, 0,  "poison: %d/%d (%c)", 
+	mvwprintw(score, 3, 1,  "poison: %d/%d (%c)", 
 			db.get_poison_counter(), 
 			db.get_poison_qual(),
 			db.get_poison_ok() ? 'O' : 'X'
 			);
-	mvwprintw(score, 3, 0,  "gate: %d/%d (%c)", 
+	mvwprintw(score, 4, 1,  "gate: %d/%d (%c)", 
 			db.get_gate_counter(), 
 			db.get_gate_qual(),
 			db.get_gate_ok() ? 'O' : 'X'
 			);
-	mvwprintw(score, 4,0,  "Time : %ld", Util::get_time());
-	mvwprintw(score, 5,0,  "Last input : %d", db.get_last_direction());
+	//mvwprintw(score, 4,0,  "Time : %ld", Util::get_time());
+	//mvwprintw(score, 5,0,  "Last input : %d", db.get_last_direction());
+	wborder(score, '#','#','#','#','#','#','#','#');
 	wrefresh(score);
 }
 
